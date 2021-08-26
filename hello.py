82d2136de16e32cbd3a58d53a2ba1c2f3de82690
Alexandre CUER <alexandre.cuer@cerema.fr>
2021-08-26 21:32:07 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,24 +4,25 @@ print("hello")
 
 print("my friend")
 try:
-   import pymodbus
+    import pymodbus
 except Exception as e:
-   print(e)
+    print(e)
 else:
-   print("pymodbus is here")
+    print("pymodbus is here")
  
 try:
-   import numpy as np
+    import numpy as np
 except Exception as e:
-   print(e)
+    print(e)
 else:
-   print("numpy is here")
+    print("numpy is here")
 
 try:
-   import redis
+    import redis
 except Exception as e :
-   print(e)
+    print(e)
 else:
+    print("redis-py also :-)")
     r = redis.Redis(host="localhost", port=6379, db=0)
     promux = r.hmget("feed:1","value", "time")
     value = promux[0].decode()
